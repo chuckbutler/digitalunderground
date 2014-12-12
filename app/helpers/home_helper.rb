60ccf5e49fd2253997f6554f7bfe8b75396b9d9a
Charles Butler <chuck@dasroot.net>
2014-12-12 05:38:35 -0500

    Compute video thumbnails on the fly

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,10 @@
 module HomeHelper
   def video_image(video)
-    v = VideoInfo.new(video.embed)
+    v = VideoInfo.new(video.link)
     return v.thumbnail_medium
   end
   def video_title(video)
-    v = VideoInfo.new(video.embed)
+    v = VideoInfo.new(video.link)
     return v.title
   end
 
