0fcf794588f63cdb6b063ab7afe66099817051e9
Charles Butler <chuck@dasroot.net>
2014-12-16 01:06:05 -0500

    Adds datasources for home widgets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @video = Video.where({active: true}).limit(5)
-    @news = News.order_by(:created_at.desc)
+    # @video = Video.where({active: true}).limit(5)
+    @news = News.order_by(:created_at.desc).limit(5)
     @featured = Venue.where({feature: true}).order_by(:created_at.desc)
     @latest = Venue.all.limit(10)
+    @djs = Dj.all().order_by(:created_at.desc).limit(10)
   end
 end
