300efb7e3a18dd0788f7bebe6c4b1c254e4fde9b
Charles Butler <chuck@dasroot.net>
2014-12-16 00:35:35 -0500

    refactoring the playlists on index and show of venues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @video = Video.where({active: true}).limit(5)
     @news = News.order_by(:created_at.desc)
+    @featured = Venue.where({feature: true}).order_by(:created_at.desc)
+    @latest = Venue.all.limit(10)
   end
 end
