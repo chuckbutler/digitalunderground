f699704edae02416c238b0a65f993cbc43622c2a
Charles Butler <chuck@dasroot.net>
2014-12-17 18:46:47 -0500

    this broke all kinds of stuff Revert "Another fix that wont fix phoenixs issue"
    This reverts commit fbc8fa27c0074b6b56f2cc7fa6adac6ce5c3e47f.

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# https://github.com/plataformatec/devise/issues/2949
-# Rails.application.config.action_dispatch.cookies_serializer = :json
+Rails.application.config.action_dispatch.cookies_serializer = :json
